a07a55a5127bbeb1080201a535b0be54a97a4964
허철용 <cyheo@naver.com>
2024-10-24 23:39:27 +0900

    Counter Component Creation

---
+++ src/component/Counter.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+interface CounterProps {
+    initialCount?: number;
+}
+
+const Counter: React.FC<CounterProps> = ( { initialCount = 0}) => {
+
+    const [count, setCount] = useState<number>(initialCount);
+
+    const increment = () => setCount( count + 1 );
+    const decrement = () => setCount( count - 1 );
+
+    return (
+        <div>
+            <h1>Counter: { count }</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+}
+
+export default Counter;
