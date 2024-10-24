58aa55124cad3d8791075c7b6b2b0600e9f7b302
허철용 <cyheo@naver.com>
2024-10-24 23:50:14 +0900

    Thank you

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch } from './store';
-import { increment, decrement, incrementByAmount } from './store';
+import Counter from './component/Counter';
 
 function App() {
-  // Redux 상태 가져오기
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch: AppDispatch = useDispatch();
-
   return (
     <div className="App">
-      <h1>Redux Counter</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(5))}>Increment by 5</button>
+      <h1>React + TypeScript Counter</h1>
+      <Counter initialCount={10} />
     </div>
   );
 }
